04f5dc0e765a9e56b3994b8aeafab38a34b5453c
aryan001222 <93635527+aryan001222@users.noreply.github.com>
2023-08-10 15:12:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ let localStream;
 let peerConnection;
 let roomId;
 
+// Set up socket connection
+const socket = io();
 
+// Parse the URL parameters to extract the room ID
+const queryParams = new URLSearchParams(window.location.search);
+roomId = queryParams.get('room');
+
+if (roomId) {
+    startCallButton.disabled = true;
+    endCallButton.disabled = false;
+
+    joinMeeting(roomId);
+}
 startCallButton.addEventListener('click', () => {
     startCallButton.disabled = true;
     endCallButton.disabled = false;
